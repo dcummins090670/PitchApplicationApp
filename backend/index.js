d7cb34aebca6da0e76c9b6c6dd75ec1bc87c5d49
Donagh Cummins <donaghcummins@gmail.com>
2025-10-30 12:57:48 +0000

    Configured Database connection for Render & Supabase

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ require ('dotenv').config();
 const express  = require('express');
 const cors  = require ('cors');
 
-const mysql  = require('mysql2');
+//const mysql  = require('mysql2');
 
-const pkg = require('pg');
+//const pkg = require('pg');
 
 
 //import users from "./user.js"
